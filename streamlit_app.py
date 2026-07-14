113c6e987290bcee15f77b99a3b78be741c8a7c2
Zerstoererin <s3430242@stud.uni-frankfurt.de>
2026-07-14 11:40:32 +0000

    ü weg

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
-import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
 import numpy as np
 import streamlit as st
